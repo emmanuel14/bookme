c04584f6e8a35d0781808ca32c04e9824a1048af
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-06-06 09:20:36 +0100

    Updates

--- api/get_slots.php
+++ api/get_slots.php
@@ -23,7 +23,7 @@ if ($action === 'services') {
 
 // Get available time slots
 $professionalId = $_GET['professional_id'] ?? null;
-$serviceId = $_GET['service_id'] ?? null;s
+$serviceId = $_GET['service_id'] ?? null;
 $date = $_GET['date'] ?? null;
 
 if (!$professionalId || !$serviceId || !$date) {
