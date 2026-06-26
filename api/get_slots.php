87d9e86207bd1befb5469dcc8f94a764edc610e3
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-06-26 19:54:02 +0100

    Updates

--- api/get_slots.php
+++ api/get_slots.php
@@ -31,6 +31,6 @@ if (!$professionalId || !$serviceId || !$date) {
     exit;
 }
 
-$slots = getAvailableSlots($professionalId, $serviceId, $date);
+$slots = getAvailableSlots($professionalId, $servniceId, $date);
 echo json_encode(['success' => true, 'slots' => $slots]);
 ?>
