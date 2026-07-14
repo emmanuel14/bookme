d9f9dcbaf62401efe523d1cff3bfbc56457f51cb
Emmanuel <akuwudikeemmanuel03@gmail.com>
2026-07-14 16:52:39 +0100

    Updates

--- auth/login.php
+++ auth/login.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         header("Location: " . SITE_URL . "/{$result['role']}/dashboard.php");
         exit;
     } else {
-        $error = $result['message'];
+        $error = $resultdw['message'];
     }
 }
 
